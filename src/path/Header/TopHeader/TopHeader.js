0d94db3997a784c8ce3990910a51d1e9ffa8eeee
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2024-03-18 21:22:27 +0900

    header

--- src/path/Header/TopHeader/TopHeader.js
+++ src/path/Header/TopHeader/TopHeader.js
@@ -8,6 +8,12 @@ export default function TopHeader() {
                 <div class="name_line">
                     <a>TAEMIN PORTFOLIO</a>
                 </div>
+                <div class="menu_line">
+                    <a id="header_about">About me</a>
+                    <a id="header_skill">Skills</a>
+                    <a id="header_project">Projects</a>
+                    <a id="header_career">Career</a>
+                </div>
             </section>
         </header>
     </>
