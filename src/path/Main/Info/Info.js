f13769ea42c7d4d0a6d5965ced2d289180fced48
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2024-07-27 17:17:49 +0900

    -3

--- src/path/Main/Info/Info.js
+++ src/path/Main/Info/Info.js
@@ -13,8 +13,22 @@ export default function Info(){
             </div>
             {/* right */}
             <div className="Info_right">
-                <span className="info_Text">
+                <span className="info_text">
                     <p>이름 : 김태민</p>
+                    <p>생년월일 : 1997.12.18(28)</p>
+                    <p>주소 : 경기도 고양시 일산서구</p>
+                    <p>경력 : 2년</p>
+                    <p>tel : 010-3633-3628</p>
+                    <p>e-mail : ktm3628@naver.com</p>
+                </span>
+                <span className="edu_text">
+                    <ul>
+                        <li>
+                            <h3>교육사항</h3>
+                            <p>코리아 it 정보교육 기관</p>
+                            
+                        </li>
+                    </ul>
                 </span>
             </div>
         </div>
