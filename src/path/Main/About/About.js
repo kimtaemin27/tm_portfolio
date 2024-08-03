eb1aa009640ce2fe7c67a39226bb7a386199895d
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2024-08-03 14:27:55 +0900

    24-08-03

--- src/path/Main/About/About.js
+++ src/path/Main/About/About.js
@@ -1,14 +1,29 @@
 import './About.css';
+import React from 'react';
+import { motion } from 'framer-motion';
+import { useInView } from "react-intersection-observer";
 
 export default function About(){
+
+    const { ref, inView } = useInView({
+        triggerOnce: false,
+        threshold: 0.5,
+    });
+
     return <>
-        <div className="About_outter">
-            {/* left */}
-            <div className="left">
-                <p className="firstRow">풀지 못할 오류는 없다</p>
-                <p className="secondRow">모든 오류에는 해답이 있듯이</p>
-                <p className="elseRow">앞으로 새롭게 맞이하는 문제와 오류들에 제 끈기와 열정으로 도전해 나가겠습니다.</p>
+        <motion.div 
+            initial={{ opacity: 0, backGround: "red" }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.5, delay: 1 }}
+        >
+            <div className="About_outter">
+                {/* left */}
+                <div className="left">
+                    <p className="firstRow">풀지 못할 오류는 없다</p>
+                    <p className="secondRow">모든 오류에는 해답이 있듯이</p>
+                    <p className="elseRow">앞으로 새롭게 맞이하는 문제와 오류들에 제 끈기와 열정으로 도전해 나가겠습니다.</p>
+                </div>
             </div>
-        </div>
+        </motion.div>
     </>
 }
